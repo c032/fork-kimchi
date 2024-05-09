4b663f91b4bc7bb1a7d8257a436ef4c552f7875b
Simon Ser <contact@emersion.fr>
2024-05-09 09:02:53 +0200

    Remove RLIMIT_NOFILE bump
    Starting with Go 1.19 [0] the file limit is increased by default.
    
    [0]: https://github.com/golang/go/commit/8427429c592588af8c49522c76b3e0e0e335d270

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,10 +13,6 @@ var configPath = "/etc/kimchi/config"
 func main() {
 	flag.StringVar(&configPath, "config", configPath, "configuration file")
 	flag.Parse()
-
-	if err := bumpOpenedFileLimit(); err != nil {
-		log.Printf("failed to bump max number of opened files: %v", err)
-	}
 
 	srv := NewServer()
 	if err := loadConfig(srv, configPath); err != nil {
@@ -52,16 +47,4 @@ func main() {
 			log.Print("config reloaded")
 		}
 	}
-}
-
-func bumpOpenedFileLimit() error {
-	var rlimit syscall.Rlimit
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		return fmt.Errorf("failed to get RLIMIT_NOFILE: %v", err)
-	}
-	rlimit.Cur = rlimit.Max
-	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		return fmt.Errorf("failed to set RLIMIT_NOFILE: %v", err)
-	}
-	return nil
 }
